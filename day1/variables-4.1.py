155205a885d0f12970944bef133d648faf403ef0
rwhiffen <rich@whiffen.org>
2022-01-10 16:18:12 -0500

    getting started with Python class.  Gonna learn some things

---
+++ day1/variables-4.1.py
@@ -0,0 +1,21 @@
+# Rich Whiffen - 1/10/2022
+# Udemy 100 Days of Code: The complete Python Pro Bootcamp for 2022
+# Day 1 - assignment 1.14
+# 🚨 Don't change the code below 👇# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = b
+b = a
+a = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
