42929531e8b03dabc55356562d32aaa030e5ef75
rwhiffen <rich@whiffen.org>
2022-09-19 11:43:54 -0400

    Day 23 done - reconsiled with course solution

--- day23/car_manager.py
+++ day23/car_manager.py
@@ -1,7 +1,34 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-
+DIFFICULTY_LEVEL=6 # smaller numbers means harder
 
 class CarManager:
-    pass
+    # class to randomly create new cars on the Road
+    # also move all cars (loop through)
+    # also increase the speed if warrented
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        random_chance = random.randint(1, DIFFICULTY_LEVEL)
+        # 1 in DIFFICULTY_LEVEL chance of a new car coming
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250) # pick hight
+            new_car.goto(300, random_y) #place car
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
