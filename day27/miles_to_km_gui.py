1881044bb79423c36f3160fa4c601e9225eebb16
rwhiffen <rich@whiffen.org>
2022-10-06 17:06:33 -0400

    switching to round() to match class output

--- day27/miles_to_km_gui.py
+++ day27/miles_to_km_gui.py
@@ -10,7 +10,7 @@ from tkinter import *
 
 def button_clicked():
     miles = float(miles_input.get())
-    km = miles * 1.609
+    km = round(miles * 1.609)
     km_result_label.config(text=f"{km}")
 
 
