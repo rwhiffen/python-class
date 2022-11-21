c96012e3f355e05fd6fe424516326a4881d2401c
rwhiffen <rich@whiffen.org>
2022-11-21 17:42:58 -0500

    fixing indent error in template

--- secrets-template.py
+++ secrets-template.py
@@ -1,6 +1,6 @@
 # dictionary template to copy for what ever secrets you're storing
-    secrets = {
-    'ssid' : 'your_wifi_ssid',
-    'password' : 'your_wifi_password',
-    'api_token' : 'xxxxxxxxxxxxxxxxxxxxxxxx'
-	}
+secrets = {
+'ssid' : 'your_wifi_ssid',
+'password' : 'your_wifi_password',
+'api_token' : 'xxxxxxxxxxxxxxxxxxxxxxxx'
+}
