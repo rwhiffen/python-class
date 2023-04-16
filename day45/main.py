f4b668aa4982ddc1ac977c6b29e4660af5f82381
rwhiffen <rich@whiffen.org>
2023-04-15 20:12:10 -0400

    Finishing day45 after months off.

--- day45/main.py
+++ day45/main.py
@@ -4,6 +4,8 @@
 # Udemy 100 Days of Code: The complete Python Pro Bootcamp for 2022
 # Day 45 - using BeautifulSoup to scrape websites.
 #
+# picking this back up in April (yikes) - didn't change much just
+# reviewing this lesson to jump into the next.
 
 from bs4 import BeautifulSoup
 
@@ -12,7 +14,9 @@ with open("website.html") as file:
 
 soup = BeautifulSoup(contents, "html.parser")
 
+print("title html")
 print(soup.title)
+print("title string")
 print(soup.title.string)
 
 
@@ -25,5 +29,14 @@ all_a_tags = soup.find_all(name="a")
 
 print(all_a_tags)
 
+print ("loop through the href tags")
 for tag in all_a_tags:
     print(tag.get("href"))
+
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+#print the value of a variable - need to review what the _ is for in class_
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading.get("class"))
