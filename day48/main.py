25fdbfd86d4985b9cd808503a4ff5491eeab87c6
rwhiffen <rich@whiffen.org>
2023-04-17 13:13:28 -0400

    stubbing in day47

---
+++ day48/main.py
@@ -0,0 +1,9 @@
+
+# Rich Whiffen - 4/16/2023
+#
+# Udemy 100 Days of Code: The complete Python Pro Bootcamp for 2022
+# Day 48 - using Selenium 
+
+from selenium import webdriver
+
+chrome_driver="/Users/RWhiff000/Applications/chromedriver_mac_arm64/chromedriver"
