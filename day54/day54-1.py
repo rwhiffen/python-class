44c87387994f747238f02e3b531b52768096c272
rwhiffen <rich@whiffen.org>
2023-06-14 16:43:13 -0400

    updating day 54

--- day54/day54-1.py
+++ day54/day54-1.py
@@ -3,3 +3,10 @@
 # Udemy 100 Days of Code: The complete Python Pro Bootcamp for 2022
 # Day 54 - FLASK with Python
 #
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
---
+++ day54/day54-2.py
@@ -0,0 +1,7 @@
+# Rich Whiffen - 6/14/2023
+#
+# Udemy 100 Days of Code: The complete Python Pro Bootcamp for 2022
+# Day 54 - FLASK with Python
+#
+
+# decorators
